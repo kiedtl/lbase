745351b02b8937ea71c2ec33fe80d31eed15668f
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2020-01-21 22:23:21 -0500

    misc: update version string

--- common.h
+++ common.h
@@ -6,7 +6,7 @@
 #include "types.h"
 #define LBASE_VERSION  "0.0.1"
 
-#define VERSION(n)    fprintf(stdout, "lbase->%s v%s\n", n, LBASE_VERSION);
+#define VERSION(n)    fprintf(stdout, "%s (lbase) v%s\n", n, LBASE_VERSION);
 #define EPRINT(...)   fprintf(stderr, __VA_ARGS__);
 
 usize intlen(u64 i);
