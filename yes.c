09f2e3b25939a9640a602410728e2c88982a096f
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2020-01-21 21:01:32 -0500

    yes: fix

--- yes.c
+++ yes.c
@@ -1,36 +1,48 @@
 #include "common.h"
-#include <stdio.h>
+#include <unistd.h>
 #include "yes.h"
 #include "types.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int
 main(int argc, char *argv[])
 {
-	if (argc < 2) {
-		yes("y");
-		return 0;
+	if (argc > 1) {
+		if (strcmp(argv[1], "--help") == 0
+			|| strcmp(argv[1], "-h") == 0) {
+			help();
+			return 0;
+		} else if (strcmp(argv[1], "--version") == 0
+			|| strcmp(argv[1], "-v") == 0) {
+			VERSION(NAME);
+			return 0;
+		}
 	}
-
-	if (strcmp(argv[1], "--help") == 0
-		|| strcmp(argv[1], "-h") == 0) {
-		help();
-		return 0;
-	} else if (strcmp(argv[1], "--version") == 0
-		|| strcmp(argv[1], "-v") == 0) {
-		VERSION(NAME);
-		return 0;
-	} else
-		yes(argv[1]);
-
-	return 0;
+	
+	if (argc < 2) yes("y\n", 2);
+	else {
+		usize size = strlen(argv[1]) + 1;
+		char string[size];
+		strcpy(string, argv[1]);
+		string[size - 1] = '\n';
+		yes(string, size);
+	}
 }
 
 void
-yes(char *string)
+yes(char *string, usize size)
 {
-	for (;;) puts(string);
+	char *buf = malloc(SIZE);
+	usize bufused = 0;
+
+	while (bufused < SIZE) {
+		memcpy(buf + bufused, string, size);
+		bufused += size;
+	}
+
+	while (write(1, buf, SIZE));
 }
 
 void
--- yes.h
+++ yes.h
@@ -1,10 +1,18 @@
+/*
+ * Thanks to the author of the following Reddit post:
+ * https://old.reddit.com/r/unix/comments/6gxduc/how_is_gnu_yes_so_fast/
+ * It provides a good overview on the speed of GNU yes.
+ */
+
 #ifndef YES_H
 #define YES_H
 
-#define NAME "yes"
+#include <stdio.h>
+#define NAME  "yes"
+#define SIZE  BUFSIZ * 2
 
 /* function prototypes */
-void yes(char *string);
+void yes(char *string, usize size);
 void help(void);
 
 #endif
